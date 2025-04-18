5e0381004ef8e9d576fec42aa4ff4a24114be680
Feryadi <feryadi@Feryadis-MacBook-Pro.local>
2025-04-18 19:54:29 +0700

    initial commit :sparkle:

---
+++ src/main/java/dev/feryadialoi/fluenteither/FluentEitherApplication.java
@@ -0,0 +1,13 @@
+package dev.feryadialoi.fluenteither;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluentEitherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluentEitherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/feryadialoi/fluenteither/FluentEitherApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.feryadialoi.fluenteither;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluentEitherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
